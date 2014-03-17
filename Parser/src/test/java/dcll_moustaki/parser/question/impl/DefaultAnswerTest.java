0e34e872fb2d720f2d47bc56bf0769be6b537e21
Fabien Fac <rolfo.fabien@gmail.com>
2014-03-17 15:31:16 +0100

    couverture à 100% de DefaultQuestion

--- Parser/src/test/java/dcll_moustaki/parser/question/impl/DefaultAnswerTest.java
+++ Parser/src/test/java/dcll_moustaki/parser/question/impl/DefaultAnswerTest.java
@@ -109,11 +109,17 @@ public class DefaultAnswerTest extends TestCase {
 		String identifier = "identifier";
 		String identifier2 = "identifier2";
 		DefaultAnswer answer = new DefaultAnswer();
+		DefaultAnswer answer3 = null;
 		answer.setIdentifier(identifier);
 		
 		DefaultAnswer answer2 = new DefaultAnswer();
 		answer2.setIdentifier(identifier2);
 		
-		assertNotSame(answer, answer2);
+		assertFalse(answer.equals(answer2));
+		
+		assertFalse(answer.equals(new String("oij")));
+		
+		assertFalse(answer.equals(answer3));
+		
 	}
 }
